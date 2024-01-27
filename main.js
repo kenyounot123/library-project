25957014c3a0855d29627ded7629079e8ff50c80
kenyounot123 <kenlu519@gmail.com>
2024-01-27 14:26:16 -0500

    Create form modal

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
 const myLibrary = [];
 
-function Book() {
-
+function Book(title, author, pages, status) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.status = status
 }
 function addBookToLibrary() {
-  
+  const title = document.getElementById('title').value 
+  const author = document.getElementById('author').value 
+  const pages = document.getElementById('pages').value
+  const status = document.getElementById('status').checked
 }
+
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector(".new-book-btn");
+const closeButton = document.querySelector("dialog button");
+showButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
